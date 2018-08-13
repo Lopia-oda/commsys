583c1d21683e154a1af2e705313d6afc74fd8f11
Lopia-oda <t.oda@lopia.jp>
2018-08-13 17:55:51 +0900

    DB access add

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Dicker;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $dickers = Dicker::all();
+        return view('home', ['dickers' => $dickers]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,8 +3,17 @@
 @section('content')
     <div class="container">
         <div class="card">
-            <div class="card-header">提案一覧</div>
-
+            <div class="card-header">
+                <div class="float-left">
+                    <button type="button" class="btn btn-outline-secondary btn-sm">申請中</button>
+                    <button type="button" class="btn btn-primary btn-sm">商談中</button>
+                    <button type="button" class="btn btn-outline-secondary btn-sm">商談終了</button>
+                    <button type="button" class="btn btn-outline-secondary btn-sm">申請却下</button>
+                </div>
+                <div class="float-right">
+                    <button type="button" class="btn btn-danger btn-sm">新規商談作成</button>
+                </div>
+            </div>
             <div class="card-body">
                 @if (session('status'))
                     <div class="alert alert-success" role="alert">
@@ -13,76 +22,25 @@
                 @endif
 
                 <table class="table table-hover table-responsive container">
-                    <!--roop-->
+                    @foreach ($dickers as $dicker)
                         <tr>
                             <td>
                                 <img src="no-image.jpg" class="img-rounded"><br>
                                 <div class="text-nowrap text-center"><b>提案中</b></div>
                             </td>
                             <td>
-                                <div class="row ">
-                                    <div class="col-lg-12"><b>仕入先１</b></div>
-                                    <div class="col-lg-12 text-primary"><b>明星『一平ちゃん夜店の焼きそば』</b></div>
-                                    <div class="col-xs-12 col-sm-6 col-lg-3" style="background-color:lemonchiffon;">JAN：4902881419291</div>
-                                    <div class="col-xs-12 col-sm-6 col-lg-3" style="background-color:lemonchiffon;">期限：2018/10/01 12:00</div>
-                                    <div class="col-xs-12 col-sm-6 col-lg-3" style="background-color:lemonchiffon;">提案数量：1,000</div>
-                                    <div class="col-xs-12 col-sm-6 col-lg-3" style="background-color:lemonchiffon;">残数量：<font color="red"><b>550</b></font></div>
-                                    <div class="col-12">メモメモメモメモメモメ モメモメモメモメモメモメモメモメモメモメモメモメモ</div>
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <img src="no-image.jpg" class="img-rounded"><br>
-                                <div class="text-nowrap text-center"><b>申請中</b></div>
-                            </td>
-                            <td>
                                 <div class="row">
-                                    <div class="col-lg-12"><b>仕入先１</b></div>
-                                    <div class="col-lg-12 text-primary"><b>桃屋『ごはんですよ！』</b></div>
-                                    <div class="col-xs-12 col-sm-6 col-lg-3" style="background-color:lemonchiffon;">JAN：</div>
-                                    <div class="col-xs-12 col-sm-6 col-lg-3" style="background-color:lemonchiffon;">期限：2018/09/30 12:00</div>
-                                    <div class="col-xs-12 col-sm-6 col-lg-3" style="background-color:lemonchiffon;">提案数量：500</div>
-                                    <div class="col-xs-12 col-sm-6 col-lg-3" style="background-color:lemonchiffon;">残数量：<font color="red"><b>250</b></font></div>
-                                    <div class="col-12">メモメモメモメモメモメモメモメモメモメモメモメモメモメモメモ</div>
+                                    <div class="col-lg-12"><b>{{ $dicker->user_id }}</b></div>
+                                    <div class="col-lg-12 text-primary"><b>{{ $dicker->trade_name }}</b></div>
+                                    <div class="col-xs-12 col-sm-6 col-lg-3" style="background-color:lemonchiffon;">JAN：{{ $dicker->jan_code }}</div>
+                                    <div class="col-xs-12 col-sm-6 col-lg-3" style="background-color:lemonchiffon;">期限：{{ $dicker->dicker_limit_at }}</div>
+                                    <div class="col-xs-12 col-sm-6 col-lg-3" style="background-color:lemonchiffon;">提案数量：{{ $dicker->stock_quantity }}</div>
+                                    <div class="col-xs-12 col-sm-6 col-lg-3" style="background-color:lemonchiffon;">残数量：<font color="red"><b>{{ $dicker->remaining_quantity }}</b></font></div>
+                                    <div class="col-12">{{ $dicker->memo }}</div>
                                 </div>
                             </td>
                         </tr>
-                        <tr>
-                            <td>
-                                <img src="no-image.jpg" class="img-rounded"><br>
-                                <div class="text-nowrap text-center"><b>商談終了</b></div>
-                            </td>
-                            <td>
-                                <div class="row">
-                                    <div class="col-lg-12"><b>仕入先１</b></div>
-                                    <div class="col-lg-12 text-primary"><b>カルビー『ポテトチップスコンソメ味』</b></div>
-                                    <div class="col-xs-12 col-sm-6 col-lg-3" style="background-color:lemonchiffon;">JAN：4901330502881</div>
-                                    <div class="col-xs-12 col-sm-6 col-lg-3" style="background-color:lemonchiffon;">期限：2018/10/15 12:00</div>
-                                    <div class="col-xs-12 col-sm-6 col-lg-3" style="background-color:lemonchiffon;">提案数量：250</div>
-                                    <div class="col-xs-12 col-sm-6 col-lg-3" style="background-color:lemonchiffon;">残数量：<font color="red"><b>0</b></font></div>
-                                    <div class="col-12">メモメモメモメモメモメモメモメモメモメモメモメモメモメモメモメモメモメモメモメモメモメモメモメモメモメモメモメモメモメモメモメモメモメモメモメモメモメモメモ</div>
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <img src="no-image.jpg" class="img-rounded"><br>
-                                <div class="text-nowrap text-center"><b>申請却下</b></div>
-                            </td>
-                            <td>
-                                <div class="row">
-                                    <div class="col-lg-12"><b>仕入先１</b></div>
-                                    <div class="col-lg-12 text-primary"><b>はごろもフーズ『スイートコーン缶詰』６缶ケース</b></div>
-                                    <div class="col-xs-12 col-sm-6 col-lg-3" style="background-color:lemonchiffon;">JAN：4902560226066</div>
-                                    <div class="col-xs-12 col-sm-6 col-lg-3" style="background-color:lemonchiffon;">期限：2018/07/07 12:00</div>
-                                    <div class="col-xs-12 col-sm-6 col-lg-3" style="background-color:lemonchiffon;">提案数量：2,000</div>
-                                    <div class="col-xs-12 col-sm-6 col-lg-3" style="background-color:lemonchiffon;">残数量：<font color="red"><b>2,000</b></font></div>
-                                    <div class="col-12">メモメモメモメモメモメモメモメモメモメモメモ</div>
-                                </div>
-                            </td>
-                        </tr>
-                    <!--roop end-->
+                    @endforeach
                 </table>
             </div>
         </div>
