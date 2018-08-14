83e238d3892fd13093be1757b3c31cf896057dec
Lopia-oda <t.oda@lopia.jp>
2018-08-14 18:03:02 +0900

    新規作成画面追加

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,11 +7,11 @@
                 <div class="float-left">
                     <button type="button" class="btn btn-outline-secondary btn-sm">申請中</button>
                     <button type="button" class="btn btn-primary btn-sm">商談中</button>
-                    <button type="button" class="btn btn-outline-secondary btn-sm">商談終了</button>
                     <button type="button" class="btn btn-outline-secondary btn-sm">申請却下</button>
+                    <button type="button" class="btn btn-outline-secondary btn-sm">商談終了</button>
                 </div>
                 <div class="float-right">
-                    <button type="button" class="btn btn-danger btn-sm">新規商談作成</button>
+                    <a href="{{ route('dicker_edit') }}" type="button" class="btn btn-danger btn-sm">新規商談</a>
                 </div>
             </div>
             <div class="card-body">
@@ -25,17 +25,17 @@
                     @foreach ($dickers as $dicker)
                         <tr>
                             <td>
+                                <div class="text-nowrap text-center"><b>商談中</b></div>
                                 <img src="no-image.jpg" class="img-rounded"><br>
-                                <div class="text-nowrap text-center"><b>提案中</b></div>
                             </td>
                             <td>
                                 <div class="row">
                                     <div class="col-lg-12"><b>{{ $dicker->user_id }}</b></div>
                                     <div class="col-lg-12 text-primary"><b>{{ $dicker->trade_name }}</b></div>
                                     <div class="col-xs-12 col-sm-6 col-lg-3" style="background-color:lemonchiffon;">JAN：{{ $dicker->jan_code }}</div>
-                                    <div class="col-xs-12 col-sm-6 col-lg-3" style="background-color:lemonchiffon;">期限：{{ $dicker->dicker_limit_at }}</div>
+                                    <div class="col-xs-12 col-sm-6 col-lg-3" style="background-color:lemonchiffon;">期限：{{ $dicker->dicker_limit_at->format('Y/m/d H:i') }}</div>
                                     <div class="col-xs-12 col-sm-6 col-lg-3" style="background-color:lemonchiffon;">提案数量：{{ $dicker->stock_quantity }}</div>
-                                    <div class="col-xs-12 col-sm-6 col-lg-3" style="background-color:lemonchiffon;">残数量：<font color="red"><b>{{ $dicker->remaining_quantity }}</b></font></div>
+                                    <div class="col-xs-12 col-sm-6 col-lg-3" style="background-color:lemonchiffon;">残数量：<span style="color:red;"><b>{{ $dicker->remaining_quantity }}</b></span></div>
                                     <div class="col-12">{{ $dicker->memo }}</div>
                                 </div>
                             </td>
